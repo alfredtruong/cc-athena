8567fd1e4349a75a54fbf336ddc6f690b385edc6
alfredtruong <alfred.truong@gmail.com>
2024-07-31 10:19:58 +0800

    clean up athena code

---
+++ athena.py
@@ -0,0 +1,110 @@
+import os
+import pandas as pd
+from pathlib import Path
+import re
+import hashlib
+from sqlalchemy.engine import create_engine
+from sqlalchemy import text
+from urllib.parse import quote_plus
+
+
+AWS_KEY = os.getenv('AWS_KEY')
+AWS_SECRET = os.getenv('AWS_SECRET')
+
+class AthenaQuery:
+  '''
+  cached AWS Athena queries
+  '''
+  def __init__(self, path: str = 'data/queries/', testing: bool = False) -> None:
+    self.conn_str = 'awsathena+rest://{aws_access_key_id}:{aws_secret_access_key}@athena.{region_name}.amazonaws.com:443/{schema_name}?s3_staging_dir={s3_staging_dir}'
+
+    self.engine = create_engine(
+      self.conn_str.format(
+        aws_access_key_id=quote_plus(AWS_KEY),
+        aws_secret_access_key=quote_plus(AWS_SECRET),
+        region_name='us-east-1',
+        schema_name='ccindex',
+        s3_staging_dir=quote_plus('s3://omgbananarepublic/')
+      )
+    )
+
+    self.query_output_path = Path(path)
+
+    self.testing = testing
+
+  def touch_filepath(self, _hash:str) -> Path:
+    return self.query_output_path / f'{_hash}'
+
+  def results_filepath(self, _hash:str) -> Path:
+    return self.query_output_path / f'{_hash}_results.pkl'
+
+  def query_filepath(self, _hash:str) -> Path:
+    return self.query_output_path / f'{_hash}_query.txt'
+
+  def strip_query(self, query:str) -> str:
+    query = re.sub(r'--.*$', '', query, flags=re.MULTILINE)  # remove comments
+    query = re.sub(r'\s+', ' ', query) # Remove whitespace
+    return query.strip()
+  '''
+  stripped_query = strip_query(query)
+  '''
+
+  def hash_query(self, query:str) -> str:
+    stripped_query = self.strip_query(query) # standardize
+    #hasher = hashlib.sha256() # longer hashes
+    hasher = hashlib.md5() # shorter hashes
+    hasher.update(stripped_query.encode('utf-8'))
+    return hasher.hexdigest()
+  '''
+  hash_query(query)
+  '''
+
+  def query_athena(self, query:str) -> pd.DataFrame:
+    print('query AWS')
+    return pd.read_sql(text(query),self.engine)
+
+  def do_query(self, query:str) -> pd.DataFrame:
+    _hash = self.hash_query(query)
+    results_fp = self.results_filepath(_hash)
+    if results_fp.exists():
+      print(f'read cached results for {_hash}')
+      return pd.read_pickle(results_fp)
+    else:
+      # ensure output directory exists
+      query_fp = self.query_filepath(_hash)
+      if not query_fp.parent.exists():
+        query_fp.mkdir(parents=True, exist_ok=True)
+
+      # is testing or not
+      if self.testing:
+        print('would have done a query')
+        return
+      
+      # do query
+      df = self.query_athena(query)
+
+      # save query
+      df.to_pickle(results_fp)
+
+      # save hash_query.txt
+      with open(query_fp,'w') as f:
+        f.write(query)
+
+      # save touch file
+      Path(self.touch_filepath(_hash)).touch()
+
+      # logging
+      print(_hash)
+
+      # return results
+      return df
+
+  def load_cached_result(self, _hash:str) -> pd.DataFrame:
+      # show query
+      with open(self.query_filepath(_hash),'r',encoding='utf-8') as f:
+        for line in f.readlines():
+          print(line,end='')
+
+      # return results
+      print('read cached results')
+      return pd.read_pickle(self.results_filepath(_hash))
--- query_sqlalchemy.py
+++ query_sqlalchemy.py
@@ -6,15 +6,13 @@
 # (2) boto3                 # fast but harder to setup
 #   - https://realpython.com/python-boto3-aws-s3/#:~:text=Boto3%20is%20the%20name%20of,resources%20from%20your%20Python%20scripts
 
+# https://groups.google.com/g/common-crawl/c/t_H0yeL26eY/m/INT0uh7R5-IJ
+# https://github.com/brevityinmotion/straylight/blob/main/notebooks/corefunctions.ipynb
+# https://github.com/brevityinmotion/straylight/blob/main/notebooks/tools-commoncrawl.ipynb
+
 #%%
-import os
-from pathlib import Path
 import pandas as pd
-import re
-import hashlib
-from sqlalchemy.engine import create_engine
-from sqlalchemy import text
-from urllib.parse import quote_plus
+from athena import AthenaQuery
 
 pd.set_option('display.max_colwidth', None)
 
@@ -22,109 +20,11 @@ pd.set_option('display.max_colwidth', None)
 ########################################
 # sqlalchemy
 ########################################
-AWS_KEY = os.getenv('AWS_KEY')
-AWS_SECRET = os.getenv('AWS_SECRET')
 
 #%%
 
-class AthenaQuery:
-  '''
-  cached AWS Athena queries
-  '''
-  def __init__(self, path: str = 'data/queries/', testing: bool = False) -> None:
-    self.conn_str = 'awsathena+rest://{aws_access_key_id}:{aws_secret_access_key}@athena.{region_name}.amazonaws.com:443/'\
-           '{schema_name}?s3_staging_dir={s3_staging_dir}'
+INDEX = '2024-26'
 
-    self.engine = create_engine(
-      self.conn_str.format(
-        aws_access_key_id=quote_plus(AWS_KEY),
-        aws_secret_access_key=quote_plus(AWS_SECRET),
-        region_name='us-east-1',
-        schema_name='ccindex',
-        s3_staging_dir=quote_plus('s3://omgbananarepublic/')
-      )
-    )
-
-    self.query_output_path = Path(path)
-
-    self.testing = testing
-
-  def touch_filepath(self, _hash:str) -> Path:
-    return self.query_output_path / f'{_hash}'
-
-  def results_filepath(self, _hash:str) -> Path:
-    return self.query_output_path / f'{_hash}_results.pkl'
-
-  def query_filepath(self, _hash:str) -> Path:
-    return self.query_output_path / f'{_hash}_query.txt'
-
-  def strip_query(self, query:str) -> str:
-    query = re.sub(r'--.*$', '', query, flags=re.MULTILINE)  # remove comments
-    query = re.sub(r'\s+', ' ', query) # Remove whitespace
-    return query.strip()
-  '''
-  stripped_query = strip_query(query)
-  '''
-
-  def hash_query(self, query:str) -> str:
-    stripped_query = self.strip_query(query) # standardize
-    #hasher = hashlib.sha256() # longer hashes
-    hasher = hashlib.md5() # shorter hashes
-    hasher.update(stripped_query.encode('utf-8'))
-    return hasher.hexdigest()
-  '''
-  hash_query(query)
-  '''
-
-  def query_athena(self, query:str) -> pd.DataFrame:
-    print('query AWS')
-    return pd.read_sql(text(query),self.engine)
-
-  def do_query(self, query:str) -> pd.DataFrame:
-    _hash = self.hash_query(query)
-    results_fp = self.results_filepath(_hash)
-    if results_fp.exists():
-      print(f'read cached results for {_hash}')
-      return pd.read_pickle(results_fp)
-    else:
-      # ensure output directory exists
-      query_fp = self.query_filepath(_hash)
-      if not query_fp.parent.exists():
-        query_fp.mkdir(parents=True, exist_ok=True)
-
-      # is testing or not
-      if self.testing:
-        print('would have done a query')
-        return
-      
-      # do query
-      df = self.query_athena(query)
-
-      # save query
-      df.to_pickle(results_fp)
-
-      # save hash_query.txt
-      with open(query_fp,'w') as f:
-        f.write(query)
-
-      # save touch file
-      Path(self.touch_filepath(_hash)).touch()
-
-      # logging
-      print(_hash)
-
-      # return results
-      return df
-
-  def load_cached_result(self, _hash:str) -> pd.DataFrame:
-      # show query
-      with open(self.query_filepath(_hash),'r',encoding='utf-8') as f:
-        for line in f.readlines():
-          print(line,end='')
-
-      # return results
-      print('read cached results')
-      return pd.read_pickle(self.results_filepath(_hash))
 
 aq = AthenaQuery()
 
@@ -281,7 +181,7 @@ FROM (
     warc_record_offset,                               -- where to start
     warc_record_length                                -- how far to go
   FROM "ccindex"."ccindex"
-  WHERE crawl = 'CC-MAIN-2024-26'                     -- filter by crawl
+  WHERE crawl = 'CC-MAIN-{index}'                     -- filter by crawl
     AND subset = 'warc'                               -- filter by subset
     AND url_host_tld = 'com'                          -- domain must end with .com
     AND fetch_status = 200                            -- must be successful request
@@ -297,7 +197,26 @@ GROUP BY
 ORDER BY
   FIRST(t1.warc_record_length) ASC;
 '''
-res=aq.do_query(query)
+query = r'''
+  SELECT
+    url,                                              -- full url # https://www.example.com/path/index.html
+    url_surtkey,                                      -- sortable url # com,example)/path/index.html
+    url_host_name,                                    -- url_host_name # www.example.com
+    url_host_registered_domain,                       -- short url # example.com
+    content_digest,                                   -- SHA-1 hashing of content
+    warc_filename,                                    -- which warc
+    warc_record_offset,                               -- where to start
+    warc_record_length                                -- how far to go
+  FROM "ccindex"."ccindex"
+  WHERE crawl = 'CC-MAIN-{index}'                     -- filter by crawl
+    AND subset = 'warc'                               -- filter by subset
+    AND url_host_tld = 'com'                          -- domain must end with .com
+    AND fetch_status = 200                            -- must be successful request
+    AND content_languages LIKE '%zho%'                -- must contain chinese
+    AND (content_mime_type = 'text/html' OR content_mime_detected = 'text/html') -- only care about htmls
+    AND url_host_name LIKE '%hk%'                     -- url_host_name must contain hk
+'''
+res=aq.do_query(query.format(index=INDEX))
 res
 
 
